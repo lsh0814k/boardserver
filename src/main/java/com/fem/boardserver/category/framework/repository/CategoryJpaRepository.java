938813f4edc27fda5694a786298281c6265a82bf
lsh0814k <112551128+lsh0814k@users.noreply.github.com>
2024-04-23 10:35:26 +0900

    Revert "Issue 5"

--- src/main/java/com/fem/boardserver/category/framework/repository/CategoryJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fem.boardserver.category.framework.repository;
-
-import com.fem.boardserver.category.domain.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryJpaRepository extends JpaRepository<Category, Long> {
-}
